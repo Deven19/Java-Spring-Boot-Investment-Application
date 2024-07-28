2f4154d1e55bbd41effb17f0e6d3e9e3ce9bf815
Devendra <devendra103@gmail.com>
2024-07-28 15:17:18 -0400

    Initial commit

---
+++ src/test/java/com/dc/investmentapplication/InvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.investmentapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
