bfae6067e226ff8a5025404073c699b4c41917ad
Devendra <devendra103@gmail.com>
2024-08-07 22:29:54 -0400

    Initial commit

--- src/test/java/com/dc/investmentapplication/serviceTest/UserServiceTest.java
+++ src/test/java/com/dc/investmentapplication/serviceTest/UserServiceTest.java
@@ -1,2 +1,43 @@
-package com.dc.investmentapplication.serviceTest;public class UserServiceTest {
+package com.dc.investmentapplication.serviceTest;
+
+import com.dc.investmentapplication.entity.User;
+import com.dc.investmentapplication.repository.UserRepository;
+import com.dc.investmentapplication.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+public class UserServiceTest {
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testCreateUser(){
+        User user = new User();
+        user.setUsername("test@test.com");
+        user.setPassword("password");
+        user.setUsername("test");
+
+        when(userRepository.save(user)).thenReturn(user);
+
+        try {
+            String createdUser = userService.createUser(user);
+            assertEquals("User created!", createdUser);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
